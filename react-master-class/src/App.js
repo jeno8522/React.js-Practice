40f97dcb365f37ac7b63f15a868df69c7237d45b
jeno8522 <68889383+jeno8522@users.noreply.github.com>
2022-08-08 20:47:19 +0900

    learn Theme

--- react-master-class/src/App.js
+++ react-master-class/src/App.js
@@ -1,47 +1,21 @@
 import styled, { keyframes } from "styled-components";
 
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 const Wrapper = styled.div`
   display: flex;
-`;
-
-const rotateAnimation = keyframes`
-  0% {
-    transform:rotate(0deg);
-    border-radius: 0px;
-  }
-  50%{
-    border-radius: 100px;
-  }
-  100%{
-    transform:rotate(360deg);
-    border-radius: 0px;
-  }
-`;
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  display: flex;
+  height: 100vh;
+  width: 100vw;
   justify-content: center;
   align-items: center;
-  animation: ${rotateAnimation} 1s linear infinite;
-  ${Emoji} {
-    font-size: 36px;
-    &:hover {
-      font-size: 80px;
-    }
-  }
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji>^0^</Emoji>
-      </Box>
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
