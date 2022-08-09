b1e8274da24d012d1649d6f9dadec6e2a2a8df81
jeno8522 <68889383+jeno8522@users.noreply.github.com>
2022-08-09 18:10:01 +0900

    learn typescript

---
+++ react-masterclass/src/App.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import Circle from "./Circle";
+
+function App() {
+  return (
+    <div>
+      <Circle bgColor="teal" borderColor="black" />
+      <Circle text="i'm here" bgColor="tomato" />
+    </div>
+  );
+}
+
+export default App;
